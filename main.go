3abc7252d312cd6806a9cb5eecfd866ec396fc85
aviel-lanzman <avielnzmn@gmail.com>
2022-09-06 08:03:53 +0300

    crud linked list

--- main.go
+++ main.go
@@ -1,42 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// type NodeList struct {
-// 	length     int
-// 	nextNode   string
-// 	backNode   string
-// 	getCurrent string
-// }
+type LinkedList struct {
+	list   *Node
+	length int
+}
 
 type Node struct {
-	data string
-	back *Node
+	data int
 	next *Node
 }
 
-func (n Node) nextNode() Node {
-	return *n.next
+func createNode(data int) *Node {
+	return &Node{
+		data: data,
+		next: nil,
+	}
 }
 
-func (n Node) backNode() Node {
-	return *n.back
+func (l *LinkedList) add(data int) {
+	l.list = l.addToLinkedList(l.list, data)
+	l.length++
+	return
+}
+func (l *LinkedList) addToLinkedList(node *Node, data int) *Node {
+	if node == nil {
+		node = createNode(data)
+	} else {
+		node.next = l.addToLinkedList(node.next, data)
+	}
+	return node
+
+}
+func (l *LinkedList) remove(data int) {
+	l.list = l.removeNode(l.list, data)
+	return
+}
+func (l *LinkedList) removeNode(node *Node, data int) *Node {
+	if node == nil {
+		return nil
+	} else if node.next.data == data {
+		node.next = node.next.next
+		l.length--
+		return node
+	}
+	node.next = l.removeNode(node.next, data)
+	return node
+
+}
+func (l *LinkedList) update(data int, newData int) {
+	l.list = l.updateNode(l.list, data, newData)
+	return
+}
+func (l *LinkedList) updateNode(node *Node, data int, newData int) *Node {
+	if node == nil {
+		return nil
+	} else if node.data == data {
+		node.data = newData
+		return node
+	}
+	node.next = l.updateNode(node.next, data, newData)
+	return node
+
+}
+func (l *LinkedList) DisplayList(node *Node) {
+
+	if node != nil {
+		fmt.Printf("%+v ->", node.data)
+		l.DisplayList(node.next)
+	}
+	return
 }
 
-//	func (n Node) newNode(b *Node, ne *Node, d string) Node {
-//		n.back = b
-//		n.next = ne
-//		n.data = d
-//		return n
-//	}
+func (l *LinkedList) ShowBackwards(list *Node) {
+	if list != nil {
+		l.ShowBackwards(list.next)
+		fmt.Printf("%v ->", list.data)
+	}
+	return
+}
+
 func main() {
-	node1 := &Node{data: "a"}
-	node2 := &Node{data: "b", back: node1}
-	node3 := &Node{data: "c", back: node2}
-	node1.next = node2
-	node2.next = node3
-	fmt.Println("hade node ", node1)
-	fmt.Println("nex from node 1", node1.nextNode())
-	fmt.Println("node 2", node2)
-	fmt.Println("last node ", node3)
+	link := LinkedList{}
+	link.add(1)
+	link.add(2)
+	link.add(3)
+	link.add(4)
+	link.ShowBackwards(link.list)
+	link.removeNode(link.list, 3)
+	link.update(1, 5)
+	fmt.Println()
+	link.DisplayList(link.list)
+	fmt.Println()
+	fmt.Println("list length", link.length)
 }
